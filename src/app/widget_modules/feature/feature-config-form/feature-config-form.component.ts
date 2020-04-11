ac39755ef9d3500bd862d2f71a51be28bc6f6552
Courtney Peng <courtney.peng@gmail.com>
2020-04-10 21:55:26 -0400

    yarn upgrades and removed unused import

--- src/app/widget_modules/feature/feature-config-form/feature-config-form.component.ts
+++ src/app/widget_modules/feature/feature-config-form/feature-config-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, of } from 'rxjs';
 import { catchError, debounceTime, distinctUntilChanged, map, switchMap, take, tap } from 'rxjs/operators';
