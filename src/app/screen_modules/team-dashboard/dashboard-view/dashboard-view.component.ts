6d3f1bf905c159bb431287da31665ed7c366b1ed
Zhanpeng Chen <zpeng.ch@gmail.com>
2020-04-29 11:16:26 -0400

    Fix variable name

--- src/app/screen_modules/team-dashboard/dashboard-view/dashboard-view.component.ts
+++ src/app/screen_modules/team-dashboard/dashboard-view/dashboard-view.component.ts
@@ -99,7 +99,7 @@ export class DashboardViewComponent extends DashboardComponent implements OnInit
         component: TestWidgetComponent,
         status: 'Success',
         widgetSize: 'col-xl-2',
-        configForm: TestConfigComponent
+        configForm: TestConfigFormComponent
       },
       {
         title: 'OSS',
